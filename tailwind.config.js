2acc5c7c99be6ec58f1ba24c17a56fa3cab1435a
Turbo Gordz <gordon.draine97@yahoo.com>
2022-07-11 23:02:15 -0400

    adding script

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      ms: '720px',
+      lg: '1080px',
+      xl: '1440px'
+    },
+
+
+    extend: {
+      colors: {
+        oldmauv: '#5B2333',
+        cultured: '#F7F4F3',
+        darkliver: '#564D4A',
+        cinnabar: '#F24333',
+        carbelian: '#BA1B1D',
+        aero: '#79BEEE'
+
+      },
+      lineHeight: {
+        "extra-loose": "2.5rem"
+      },
+      padding: {
+
+      }
+    }
+  },
+  plugins: [],
+}
